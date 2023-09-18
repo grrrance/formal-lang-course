7d454f8996f375ee0c9a27fd706bec234823d1a9
Grigory Aseev <grinodin7@gmail.com>
2023-09-18 11:03:40 -0700

    feat: add functions in graph_lib that return the MultiDiGraph
    Signed-off-by: Grigory Aseev <grinodin7@gmail.com>

--- project/utils/graph_lib.py
+++ project/utils/graph_lib.py
@@ -2,8 +2,10 @@ import cfpq_data
 import networkx as nx
 from typing import Tuple
 
+from networkx import MultiDiGraph
 
-def create_two_cycle_graph(
+
+def create_two_cycle_graph_and_save(
     first_count_nodes: int, second_count_nodes: int, labels: Tuple[str, str], path: str
 ) -> None:
     """
@@ -21,9 +23,7 @@ def create_two_cycle_graph(
     path: str
     A given path containing the file name, both with and without the .dot extension
     """
-    graph = cfpq_data.labeled_two_cycles_graph(
-        first_count_nodes, second_count_nodes, labels=labels
-    )
+    graph = create_two_cycle_graph(first_count_nodes, second_count_nodes, labels)
     pydot_graph = nx.nx_pydot.to_pydot(graph)
     if not path.endswith(".dot"):
         path = path + ".dot"
@@ -31,7 +31,33 @@ def create_two_cycle_graph(
         file.write(pydot_graph.to_string().replace("\n", ""))
 
 
-def get_graph_by_name(graph_name: str) -> (int, int, set):
+def create_two_cycle_graph(
+    first_count_nodes: int, second_count_nodes: int, labels: Tuple[str, str]
+) -> MultiDiGraph:
+    """
+    Creates a two cycle graph using the given numbers of vertices in two cycles, pairs of labels and return it
+
+    Parameters
+    ----------
+    first_count_nodes: int
+    number of nodes in the first cycle
+    second_count_nodes: int
+    number of nodes in the second cycle
+    labels: Tuple[str, str]
+    A pair of labels for the graph
+
+    Returns
+    -------
+    graph: MultiDiGraph
+    A directed two cycle graph class that can store multi edges
+    """
+    graph = cfpq_data.labeled_two_cycles_graph(
+        first_count_nodes, second_count_nodes, labels=labels
+    )
+    return graph
+
+
+def get_info_graph(graph_name: str) -> (int, int, set):
     """
     Given a graph name, loads a graph from the dataset and returns the number of nodes, edges and set of graph labels
 
@@ -51,12 +77,38 @@ def get_graph_by_name(graph_name: str) -> (int, int, set):
     If there is no graph with a given name in the dataset
     """
     try:
-        path_to_graph = cfpq_data.download(graph_name)
-        graph = cfpq_data.graph_from_csv(path_to_graph)
+        graph = get_graph_by_name(graph_name)
         return (
             graph.number_of_nodes(),
             graph.number_of_edges(),
             set(d["label"] for _, _, d in graph.edges(data=True)),
         )
+    except FileNotFoundError as e:
+        raise e
+
+
+def get_graph_by_name(graph_name: str) -> MultiDiGraph:
+    """
+    Given a graph name, loads a graph from the dataset and returns the MultiDiGraph
+
+    Parameters
+    ----------
+    graph_name: str
+    Name of graph
+
+    Returns
+    -------
+    graph: MultiDiGraph
+    A directed graph class from dataset that can store multi edges
+
+    Raises
+    ----------
+    FileNotFoundError
+    If there is no graph with a given name in the dataset
+    """
+    try:
+        path_to_graph = cfpq_data.download(graph_name)
+        graph = cfpq_data.graph_from_csv(path_to_graph)
+        return graph
     except FileNotFoundError as e:
         raise e
